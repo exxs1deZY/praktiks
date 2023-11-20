32c8f9a6450a04e89ccf06fc6f1c95f83202fb0e
Денис <114236213+exxs1deZY@users.noreply.github.com>
2023-11-20 14:10:21 +0300

    12.1.py

--- 12.1.py
+++ 12.1.py
@@ -6,15 +6,18 @@ class Sword:
     def attack(self):
         return f"{self.name} наносит {self.damage} урона!"
 
-class DoubleDamage:
+class DoubleDamageDecorator:
     def __init__(self, sword):
         self.sword = sword
 
     def attack(self):
         return f"{self.sword.attack()}\nДвойной урон! {self.sword.name} теперь наносит {self.sword.damage * 2} ед. урона!"
 
+@DoubleDamageDecorator
+class SwordWithDoubleDamage(Sword):
+    pass
 
 original_sw = Sword(name="Sekiro", damage=10)
-double_damage_sw = DoubleDamage(original_sw)
+double_damage_sw = SwordWithDoubleDamage(name="Sekiro Double Damage", damage=10)
 
 print(double_damage_sw.attack())
