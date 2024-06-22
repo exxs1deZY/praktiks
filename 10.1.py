752def8e0668638dab5276324741dff99293d39d
Денис <114236213+exxs1deZY@users.noreply.github.com>
2024-06-22 08:58:34 +0300

    Delete 10.1.py

--- 10.1.py
+++
@@ -1,7 +0,0 @@
-class StringUtils:
-    @staticmethod
-    def is_palindrome(word):
-        return word == word[::-1]
-
-word = "радар"
-print(f"Является ли '{word}' палиндромом? {StringUtils.is_palindrome(word)}")
