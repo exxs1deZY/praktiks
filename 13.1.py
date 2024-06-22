9922bc0033880a051cc876d59e80b8d06aafcf02
Денис <114236213+exxs1deZY@users.noreply.github.com>
2024-06-22 08:59:31 +0300

    Delete 13.1.py

--- 13.1.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Sword(models.Model):
-    name = models.CharField(max_length=100)
-    damage = models.IntegerField()
-    rarity = models.CharField(max_length=50)
